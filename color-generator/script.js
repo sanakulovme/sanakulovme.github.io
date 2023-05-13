577876db0bdb26c03cafef1308aa7cfa4af2161a
Abbosbek Sanakulov <abbossana@gmail.com>
2023-05-13 16:25:29 +0500

    New game foiz

--- color-generator/script.js
+++ color-generator/script.js
@@ -1,8 +1,31 @@
-// const rgb = document.getElementById('rgb').value;
+let rgbRow = document.getElementById('rgbColor'),
+    hslRow = document.getElementById('hslColor')
 
 
 
-// function red() {
-//     console.log(rgb)
 
+// function copyColor(p) {
+//     if (p == 'rgb') {
+//         // rgbRow.select();
+//         rgbRow.setSelectionRange(0, 99999); // For mobile devices
+
+//         navigator.clipboard.writeText(rgbRow.value);
+
+//         alert("Copied the text: " + rgbRow.value);
+//     }
 // }
+
+function copyColor() {
+    // Get the text field
+    var copyText = document.getElementById("rgbColor");
+
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+
+    // Alert the copied text
+    alert("Copied the text: " + copyText.value);
+}
---
+++ foiz/my.js
@@ -0,0 +1,126 @@
+// let fz = document.getElementById('fz'),
+//     range_disp = document.getElementById('range_disp').value;
+// let random__num
+// oninput="output_disp.value = range_disp.value + '%' , danger_p.value =  range_disp.value - 2 + '%', warning_p.value =  range_disp.value - 1 + '%', sc.value = range_disp.value + '%', wa.value =  parseInt(range_disp.value) + 1 + '%', dn.value =  parseInt(range_disp.value) + 2 + '%'"
+
+function startGame() {
+    document.getElementById("range_disp").removeAttribute("disabled");
+    document.getElementById("tryBtn").removeAttribute("disabled");
+    document.getElementById("sg").setAttribute("disabled", "");
+    // random__num = 60
+    random__num = Math.floor(Math.random() * 100);
+
+    fz.style.height = random__num + '%'
+
+
+    console.log(random__num)
+
+}
+let range_disp = document.getElementById('range_disp');
+
+let danger_p = document.getElementById('danger_p'),
+    warning_p = document.getElementById('warning_p'),
+    sc = document.getElementById('sc'),
+    wa = document.getElementById('wa'),
+    dn = document.getElementById('dn'),
+    output_disp = document.getElementById('output_disp');
+
+let h2 = document.getElementById('h2'),
+    body = document.getElementById('body');
+
+let result = document.getElementById('step'),
+    resultNum = 0,
+    rstl = document.getElementById('result'),
+    step = 0
+
+
+function range__fn() {
+    // console.log(range_disp.value)
+    output_disp.value = range_disp.value
+
+    danger_p.value = range_disp.value - 2
+    warning_p.value = range_disp.value - 1
+    sc.value = range_disp.value + '%'
+    wa.value = parseInt(range_disp.value) + 1
+    dn.value = parseInt(range_disp.value) + 2
+
+}
+
+function tryFn() {
+    document.getElementById("sg").removeAttribute("disabled");
+    document.getElementById("tryBtn").setAttribute("disabled", "");
+    document.getElementById("range_disp").setAttribute("disabled", "");
+
+    // console.log(range_disp.value)
+    // console.log(random__num)
+    h2.innerHTML = random__num + '%'
+
+
+    // ===
+
+    let dn2 = parseInt(dn.value),
+        dan2 = parseInt(danger_p.value),
+        war2 = parseInt(warning_p.value),
+        sc2 = parseInt(sc.value),
+        wa2 = parseInt(wa.value)
+
+
+    console.log(dan2, war2, sc2, wa2, dn2)
+    console.log(typeof(dn2))
+
+
+
+    if (random__num == dn2 || random__num == dan2) {
+        step += 1
+        dn.style.transform = 'scale(1.5)'
+        danger_p.style.transform = 'scale(1.5)'
+        resultNum += 3
+        result.innerHTML = resultNum
+
+        setTimeout(() => {
+            dn.style.transform = 'scale(1.0)'
+            danger_p.style.transform = 'scale(1.0)'
+        }, 3000)
+        if (step == 6) {
+            step = 0
+            rstl = innerHTML = resultNum
+        }
+    } else if (random__num == war2 || random__num == wa2) {
+        step += 1
+        wa.style.transform = 'scale(1.5)'
+        warning_p.style.transform = 'scale(1.5)'
+        resultNum += 4
+        result.innerHTML = resultNum
+        setTimeout(() => {
+            wa.style.transform = 'scale(1.0)'
+            warning_p.style.transform = 'scale(1.0)'
+        }, 3000)
+        if (step == 6) {
+            step = 0
+            rstl = innerHTML = resultNum
+        }
+    } else if (random__num == sc2) {
+        step += 1
+        sc.style.transform = 'scale(1.5)'
+        resultNum += 5
+        result.innerHTML = resultNum
+        setTimeout(() => {
+            sc.style.transform = 'scale(1.0)'
+        }, 3000)
+        if (step == 6) {
+            step = 0
+            rstl = innerHTML = resultNum
+        }
+        step += 1
+    } else {
+
+    }
+
+
+
+
+
+
+    // console.log(dan2)
+    // console.log(typeof(dn.value)) //parseInt(typeof(warning_p.value))
+}
