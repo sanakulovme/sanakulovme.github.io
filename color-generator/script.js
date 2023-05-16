19dfa8da653c33029fcc4b6fbd74fd247dc896e4
Abbosbek Sanakulov <abbossana@gmail.com>
2023-05-16 12:12:41 +0500

    new color generator

--- color-generator/script.js
+++ color-generator/script.js
@@ -1,31 +1,65 @@
-let rgbRow = document.getElementById('rgbColor'),
-    hslRow = document.getElementById('hslColor')
+let rgbBox = document.getElementById('rgb'),
+    hslBox = document.getElementById('hls'),
+    rgbSpan = document.getElementById('rgbSpan'),
+    hlsSpan = document.getElementById('hlsSpan');
 
+let red = document.getElementById('red'),
+    green = document.getElementById('green'),
+    blue = document.getElementById('blue'),
+    alfa = document.getElementById('alfa');
 
+let h_1 = document.getElementById('h_1'),
+    h_2 = document.getElementById('h_2'),
+    h_3 = document.getElementById('h_3'),
+    h_alfa = document.getElementById('h_alfa');
 
+let hexBox = document.getElementById('hex'),
+    hexcolor = document.getElementById('hexInput'),
+    hexSpan = document.getElementById('hexSpan');
 
-// function copyColor(p) {
-//     if (p == 'rgb') {
-//         // rgbRow.select();
-//         rgbRow.setSelectionRange(0, 99999); // For mobile devices
+function rgbFn() {
+    rgbBox.style.background = 'rgba(' + red.value + ',' + green.value + ',' + blue.value + ',' + alfa.value + ')'
+    rgbSpan.value = 'rgba(' + red.value + ',' + green.value + ',' + blue.value + ',' + alfa.value + ')'
+}
 
-//         navigator.clipboard.writeText(rgbRow.value);
+function hlsFn() {
+    hslBox.style.background = 'hsla(' + h_1.value + ',' + h_2.value + '%,' + h_3.value + '%,' + h_alfa.value + ')'
+    hlsSpan.value = 'hsla(' + h_1.value + ',' + h_2.value + '%,' + h_3.value + '%,' + h_alfa.value + ')'
+}
 
-//         alert("Copied the text: " + rgbRow.value);
-//     }
-// }
+function colorFn() {
+    hexBox.style.background = hexcolor.value
+    hexSpan.value = hexcolor.value
+}
 
-function copyColor() {
-    // Get the text field
-    var copyText = document.getElementById("rgbColor");
+function copyT(i) {
+    if (i == 'rgb') {
+        rgbSpan.select();
+        rgbSpan.setSelectionRange(0, 99999); // For mobile devices
 
-    // Select the text field
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); // For mobile devices
+        // Copy the text inside the text field
+        navigator.clipboard.writeText(rgbSpan.value);
 
-    // Copy the text inside the text field
-    navigator.clipboard.writeText(copyText.value);
+        // Alert the copied text
+        alert("RGBA - Rang kodi nusxalandi: " + rgbSpan.value);
+    } else if (i == 'hsl') {
+        hlsSpan.select();
+        hlsSpan.setSelectionRange(0, 99999); // For mobile devices
 
-    // Alert the copied text
-    alert("Copied the text: " + copyText.value);
+        // Copy the text inside the text field
+        navigator.clipboard.writeText(hlsSpan.value);
+
+        // Alert the copied text
+        alert("HSLA - Rang kodi nusxalandi: " + hlsSpan.value);
+    } else if (i == 'hex') {
+        hexSpan.select();
+        hexSpan.setSelectionRange(0, 99999); // For mobile devices
+
+        // Copy the text inside the text field
+        navigator.clipboard.writeText(hexSpan.value);
+
+        // Alert the copied text
+        alert("HEX - Rang kodi nusxalandi: " + hexSpan.value);
+    }
+
 }
