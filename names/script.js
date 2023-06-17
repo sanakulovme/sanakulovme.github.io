ec6a46e378fa2a8d3424ffc56a7daaa2d40e311b
Abbosbek Sanakulov <abbossana@gmail.com>
2023-06-17 20:58:23 +0500

    yangi fayl

---
+++ names/script.js
@@ -0,0 +1,13 @@
+let name = document.getElementById('name'),
+    inputName = document.getElementById('input'),
+    name_span = document.getElementById('name_span');
+
+
+
+
+
+function nameFn() {
+    if (inputName.value != '') {
+        console.log(inputName.lenght)
+    }
+}
